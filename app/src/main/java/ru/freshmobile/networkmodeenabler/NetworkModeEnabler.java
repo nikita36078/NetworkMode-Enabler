83e33686d4ac272a5e4ded72cc2babcfe3399045
Nikita <@>
2016-11-02 21:34:47 +0300

    Initial commit #2

---
+++ app/src/main/java/ru/freshmobile/networkmodeenabler/NetworkModeEnabler.java
@@ -0,0 +1,33 @@
+package ru.freshmobile.networkmodeenabler;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+
+public class NetworkModeEnabler implements IXposedHookInitPackageResources
+{
+
+	@Override
+	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable
+	{
+		if (!resparam.packageName.equals("com.android.phone"))
+		{
+			return;
+		}
+		
+		resparam.res.setReplacement("com.android.phone", "array", "enabled_networks_except_lte_choices",
+									new String[]{"3G only", "3G/2G", "2G"});
+		resparam.res.setReplacement("com.android.phone", "array", "enabled_networks_except_lte_values",
+									new String[]{"2", "0", "1"});
+		resparam.res.setReplacement("com.android.phone", "array", "enabled_networks_4g_choices",
+									new String[]{"4G/3G/2G", "3G only", "3G/2G", "2G"});
+		resparam.res.setReplacement("com.android.phone", "array", "enabled_networks_values",
+									new String[]{"9", "2", "0", "1"});
+		resparam.res.setReplacement("com.android.phone", "array", "enabled_networks_choices",
+									new String[]{"LTE/3G/2G", "3G only", "3G/2G", "2G"});
+		resparam.res.setReplacement("com.android.phone", "string", "network_wcdma_only", "3G only");
+		resparam.res.setReplacement("com.android.phone", "string", "network_wcdma_pref", "3G/2G");
+		resparam.res.setReplacement("com.android.phone", "string", "network_4g", "4G/3G/2G");
+		resparam.res.setReplacement("com.android.phone", "string", "network_lte", "LTE/3G/2G");
+	}
+
+}
