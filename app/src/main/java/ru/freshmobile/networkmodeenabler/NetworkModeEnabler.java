f5b973badd55a7efd33122ae6d892a46fdeccf5d
Nikita <@>
2016-11-08 22:37:09 +0300

    Some compatibility fixes

--- app/src/main/java/ru/freshmobile/networkmodeenabler/NetworkModeEnabler.java
+++ app/src/main/java/ru/freshmobile/networkmodeenabler/NetworkModeEnabler.java
@@ -1,5 +1,7 @@
 package ru.freshmobile.networkmodeenabler;
 
+import android.os.Build;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 
@@ -24,12 +26,14 @@ public class NetworkModeEnabler implements IXposedHookInitPackageResources
 									new String[]{"11", "9", "2", "0", "1"});
 		resparam.res.setReplacement("com.android.phone", "array", "enabled_networks_choices",
 									new String[]{"LTE only", "LTE/3G/2G", "3G only", "3G/2G", "2G"});
-		resparam.res.setReplacement("com.android.phone", "string", "network_wcdma_only", "3G only");
-		resparam.res.setReplacement("com.android.phone", "string", "network_wcdma_pref", "3G/2G");
-		resparam.res.setReplacement("com.android.phone", "string", "network_4G", "4G/3G/2G");
-		resparam.res.setReplacement("com.android.phone", "string", "network_lte", "LTE/3G/2G");
-		resparam.res.setReplacement("com.android.phone", "string", "network_4G_only", "4G only");
-		resparam.res.setReplacement("com.android.phone", "string", "network_lte_only", "LTE only");
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			resparam.res.setReplacement("com.android.phone", "string", "network_4G", "4G/3G/2G");
+			resparam.res.setReplacement("com.android.phone", "string", "network_lte", "LTE/3G/2G");
+			resparam.res.setReplacement("com.android.phone", "string", "network_wcdma_only", "3G only");
+			resparam.res.setReplacement("com.android.phone", "string", "network_wcdma_pref", "3G/2G");
+			resparam.res.setReplacement("com.android.phone", "string", "network_4G_only", "4G only");
+			resparam.res.setReplacement("com.android.phone", "string", "network_lte_only", "LTE only");
+		}
 	}
 
 }
